f224d5e184030a39ac1dae76c6ef4c2d043d9b51
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-11 14:37:50 +0700

    raw-html

--- vue-dasar/vue-project/src/Hello.vue
+++ vue-dasar/vue-project/src/Hello.vue
@@ -1,9 +1,11 @@
 <script setup>
 const say = "Hello Vue";
+const say_raw = "<h1>Hello Vue Raw</h1>";
 </script>
 
 <template>
   <h1 class="h1">{{ say }}</h1>
+  <h1 class="h1" v-html="say_raw"></h1>
 </template>
 
 <style scoped>
