ba17187a9e4c40ffd0f5b880023482093d23da79
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-11 17:02:19 +0700

    reactive-state

---
+++ vue-dasar/vue-project/src/Counter.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<template>
+  <!-- Reactive State -->
+  <h1>{{ count }}</h1>
+
+  <!-- Cara 1 -->
+  <!-- <button @click="increment">Tambah</button> -->
+
+  <!-- Cara 2 -->
+  <button v-on:click="increment">Tambah</button>
+</template>
+
+<style scoped>
+</style>
