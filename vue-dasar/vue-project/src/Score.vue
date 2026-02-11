7a8125a9564c3381754cc530bc9ccc95a1a06738
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-11 22:44:13 +0700

    show

--- vue-dasar/vue-project/src/Score.vue
+++ vue-dasar/vue-project/src/Score.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { ref } from "vue";
 
-const score = ref(75);
+const score = ref(45);
 </script>
 
 <template>
+  <!-- Element v-show akan selalu ada di DOM, hanya saja di sembunyikan -->
+  <h1 v-show="score >= 75">Score : {{ score }}</h1>
+
+  <!-- Element v-if tidak akan ada di DOM jika kondisi false -->
   <div v-if="score >= 75">
     <h3>Yeay, pretty score {{ score }}</h3>
   </div>
