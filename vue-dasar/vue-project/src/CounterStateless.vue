a92b1fd813c0300b099acff95e8eae71eb7b3617
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-12 20:08:58 +0700

    component event (emit)

--- vue-dasar/vue-project/src/CounterStateless.vue
+++ vue-dasar/vue-project/src/CounterStateless.vue
@@ -1,11 +1,15 @@
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, defineEmits } from "vue";
 
 const { name, count } = defineProps(["name", "count"]);
+
+//Emit adalah mengirim event dari child ke parent
+const emits = defineEmits(["increment"]);
 </script>
 
 <template>
   <div>
     <h1>Counter Stateless {{ name }} : {{ count }}</h1>
+    <button @click="emits('increment')">Increment {{ name }}</button>
   </div>
 </template>
--- vue-dasar/vue-project/src/MultipleCounter.vue
+++ vue-dasar/vue-project/src/MultipleCounter.vue
@@ -5,6 +5,11 @@ import Counter from "./Counter.vue";
 import CounterStateless from "./CounterStateless.vue";
 
 const count = ref(0);
+
+//Mengirim event ke child dengan emit
+const increment = () => {
+  count.value++;
+};
 </script>
 
 <template>
@@ -15,8 +20,15 @@ const count = ref(0);
     <Counter name="joko" initial-count="10" />
     <Counter name="joni" initial-count="20" />
 
-    <CounterStateless name="andre" :count="count" />
-    <CounterStateless name="andra" :count="count" />
-    <button @click="count++">Increment</button>
+    <!-- Emit cara 1 -->
+    <CounterStateless name="andre" :count="count" @increment="increment" />
+
+    <!-- Emit cara 2 -->
+    <CounterStateless
+      name="andra"
+      :count="count"
+      @increment="() => (count += 2)"
+    />
+    <button @click="increment">Increment</button>
   </div>
 </template>
