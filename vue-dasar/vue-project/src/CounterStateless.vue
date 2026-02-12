36ca8ab7870f74f68f36e41329efdb4eb77d3684
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-12 17:20:43 +0700

    one way data flow

---
+++ vue-dasar/vue-project/src/CounterStateless.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { defineProps } from "vue";
+
+const { name, count } = defineProps(["name", "count"]);
+</script>
+
+<template>
+  <div>
+    <h1>Counter Stateless {{ name }} : {{ count }}</h1>
+  </div>
+</template>
--- vue-dasar/vue-project/src/MultipleCounter.vue
+++ vue-dasar/vue-project/src/MultipleCounter.vue
@@ -1,5 +1,10 @@
 <script setup>
+import { ref } from "vue";
+
 import Counter from "./Counter.vue";
+import CounterStateless from "./CounterStateless.vue";
+
+const count = ref(0);
 </script>
 
 <template>
@@ -9,5 +14,9 @@ import Counter from "./Counter.vue";
     <!-- Walaupun component yang sama tetapi akan terisolasi antar statenya -->
     <Counter name="joko" initial-count="10" />
     <Counter name="joni" initial-count="20" />
+
+    <CounterStateless name="andre" :count="count" />
+    <CounterStateless name="andra" :count="count" />
+    <button @click="count++">Increment</button>
   </div>
 </template>
