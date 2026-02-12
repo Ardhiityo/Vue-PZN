4df22d78661d4eafdee658a33d1753ad43400a4b
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-12 14:31:23 +0700

    watch-effect

--- vue-dasar/vue-project/src/Product.vue
+++ vue-dasar/vue-project/src/Product.vue
@@ -1,21 +1,35 @@
 <script setup>
-import { ref, watch } from "vue";
+import { ref, watch, watchEffect } from "vue";
 
 const product_id = ref("product1");
 const product = ref(null);
 
-watch(
-  product_id,
-  async (new_value, old_value) => {
-    console.log("watch called");
-    const response = await fetch(`/${new_value}.json`);
+/**
+ watch(
+   product_id,
+   async (new_value, old_value) => {
+     console.log("watch called");
+     const response = await fetch(`/${new_value}.json`);
+     product.value = await response.json();
+   },
+   {
+     immediate: true, //watch akan dijalankan segera ketika component di-mount
+     //once: true, //watch hanya dijalankan satu kali setelah melakukan perubahan
+   }
+ );
+ */
+
+//watchEffect akan dijalankan segera ketika component di-mount (immediate = true secara default)
+//akan otomatis track state yang ada di dalam watchEffect
+watchEffect(async () => {
+  console.log("watchEffect called");
+  if (product_id.value) {
+    const response = await fetch(`/${product_id.value}.json`);
     product.value = await response.json();
-  },
-  {
-    immediate: true, //watch akan dijalankan segera ketika component di-mount
-    //once: true, //watch hanya dijalankan satu kali setelah melakukan perubahan
+  } else {
+    product.value = null;
   }
-);
+});
 </script>
 
 <template>
