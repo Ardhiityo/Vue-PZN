a1be1585ce9a02f23f6c5c22dfff14523992e576
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-12 14:21:19 +0700

    watcher-options

--- vue-dasar/vue-project/src/Product.vue
+++ vue-dasar/vue-project/src/Product.vue
@@ -1,15 +1,21 @@
 <script setup>
 import { ref, watch } from "vue";
 
-const product_id = ref("");
+const product_id = ref("product1");
 const product = ref(null);
 
-watch(product_id, async (new_value, old_value) => {
-  if (new_value) {
+watch(
+  product_id,
+  async (new_value, old_value) => {
+    console.log("watch called");
     const response = await fetch(`/${new_value}.json`);
     product.value = await response.json();
+  },
+  {
+    immediate: true, //watch akan dijalankan segera ketika component di-mount
+    //once: true, //watch hanya dijalankan satu kali setelah melakukan perubahan
   }
-});
+);
 </script>
 
 <template>
