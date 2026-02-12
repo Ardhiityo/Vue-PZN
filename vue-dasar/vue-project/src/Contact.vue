a8d4fa5bed88cbd532d5fa26787b092baba4e1d5
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-12 21:16:48 +0700

    component model

--- vue-dasar/vue-project/src/Contact.vue
+++ vue-dasar/vue-project/src/Contact.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { reactive } from "vue";
+import ContactForm from "./ContactForm.vue";
 
 const form = reactive({
   name: "",
@@ -14,62 +15,7 @@ const form = reactive({
 <template>
   <h1>Contact Us</h1>
 
-  <section>
-    <form action="">
-      <div>
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name" v-model="form.name" required />
-      </div>
-
-      <div>
-        <label for="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          v-model="form.email"
-          required
-        />
-      </div>
-
-      <div>
-        <label for="age">Age</label>
-        <input type="number" name="age" id="age" v-model="form.age" required />
-      </div>
-
-      <div>
-        <label for="type">Type</label>
-        <select name="type" id="type" v-model="form.type" required>
-          <option value="">Select option</option>
-          <option value="regular">Regular</option>
-          <option value="vip">VIP</option>
-        </select>
-      </div>
-
-      <div>
-        <label for="complain">Complain</label>
-        <input
-          type="checkbox"
-          name="complain"
-          id="complain"
-          v-model="form.complain"
-          required
-        />
-      </div>
-
-      <div>
-        <label for="message">Message</label>
-        <textarea
-          name="message"
-          id="message"
-          v-model="form.message"
-          required
-        ></textarea>
-      </div>
-
-      <button @click.prevent="" type="submit">Submit</button>
-    </form>
-  </section>
+  <ContactForm v-model="form" />
 
   <h1>Preview</h1>
   <section>
---
+++ vue-dasar/vue-project/src/ContactForm.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { defineModel } from "vue";
+
+const form = defineModel();
+</script>
+
+<template>
+  <section>
+    <form action="">
+      <div>
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" v-model="form.name" required />
+      </div>
+
+      <div>
+        <label for="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          v-model="form.email"
+          required
+        />
+      </div>
+
+      <div>
+        <label for="age">Age</label>
+        <input type="number" name="age" id="age" v-model="form.age" required />
+      </div>
+
+      <div>
+        <label for="type">Type</label>
+        <select name="type" id="type" v-model="form.type" required>
+          <option value="">Select option</option>
+          <option value="regular">Regular</option>
+          <option value="vip">VIP</option>
+        </select>
+      </div>
+
+      <div>
+        <label for="complain">Complain</label>
+        <input
+          type="checkbox"
+          name="complain"
+          id="complain"
+          v-model="form.complain"
+          required
+        />
+      </div>
+
+      <div>
+        <label for="message">Message</label>
+        <textarea
+          name="message"
+          id="message"
+          v-model="form.message"
+          required
+        ></textarea>
+      </div>
+
+      <button @click.prevent="" type="submit">Submit</button>
+    </form>
+  </section>
+</template>
