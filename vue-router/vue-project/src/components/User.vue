29209baae53ab7d5ad8bd26cd8166df3a64b1091
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-14 16:51:56 +0700

    vue-router: RouterLink

--- vue-router/vue-project/src/components/User.vue
+++ vue-router/vue-project/src/components/User.vue
@@ -4,6 +4,17 @@
 <template>
   <h1>Users</h1>
   <p>This is the users header</p>
+  <ul>
+    <li>
+      <RouterLink to="/users">Profile</RouterLink>
+    </li>
+    <li>
+      <RouterLink to="/users/order">Order</RouterLink>
+    </li>
+    <li>
+      <RouterLink to="/users/wishlist">Wishlist</RouterLink>
+    </li>
+  </ul>
   <RouterView />
   <p>This is the users footer</p>
 </template>
