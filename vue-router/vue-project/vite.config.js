dfc28e19888be50c1541111b9410feaf23066248
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-14 23:06:06 +0700

    vue-router: vite

--- vue-router/vue-project/vite.config.js
+++ vue-router/vue-project/vite.config.js
@@ -15,4 +15,24 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'users': [
+            './src/components/User.vue',
+            './src/components/UserFooter.vue',
+            './src/components/UserHeader.vue',
+            './src/components/UserOrder.vue',
+            './src/components/UserOrderFooter.vue',
+            './src/components/UserOrderHeader.vue',
+            './src/components/UserProfile.vue',
+            './src/components/UserWishlist.vue',
+            './src/components/UserWishlistFooter.vue',
+            './src/components/UserWishlistHeader.vue',
+          ]
+        },
+      },
+    },
+  },
 })
