09ac4e92f1fd2b8413dd94fd3112fe743119fce5
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-14 17:28:26 +0700

    vue-router: Programmatic Navigation

--- vue-router/vue-project/src/components/ProductSearch.vue
+++ vue-router/vue-project/src/components/ProductSearch.vue
@@ -1,19 +1,25 @@
 <script setup>
 import { ref, watchEffect } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
 const products = ref([]);
+const router = useRouter();
 
 // http://localhost:5173/products/search?product=sony
-let keyword = route.query.product || "";
+const query = ref(route.query.product || "");
 
 watchEffect(() => {
+  router.push({
+    query: {
+      product: query.value,
+    },
+  });
   fetch("/api/products.json")
     .then((res) => res.json())
     .then((data) => {
       products.value = data.filter((product) =>
-        product.name.toLowerCase().includes(keyword.toLowerCase())
+        product.name.toLowerCase().includes(query.value.toLowerCase())
       );
     });
 });
@@ -21,6 +27,10 @@ watchEffect(() => {
 
 <template>
   <h1>Product Search</h1>
+  <form action="">
+    <label for="keyword">Keyword</label>
+    <input type="text" name="keyword" id="keyword" v-model="query" />
+  </form>
   <ul>
     <li v-for="product in products" :key="product.id">
       {{ product.name }} - {{ product.price }}
