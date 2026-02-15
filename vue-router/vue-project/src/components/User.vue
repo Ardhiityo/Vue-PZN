33ad5f254007992a7c51bb29377b5ee9cd960824
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-15 12:21:14 +0700

    vue-router: RouterView Slot

--- vue-router/vue-project/src/components/User.vue
+++ vue-router/vue-project/src/components/User.vue
@@ -5,7 +5,8 @@
   <h1>Users</h1>
   <RouterView name="header" />
   <!-- Jika tidak memiliki name maka akan menggunakan default -->
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <component :is="Component" :request-id="$route.query.requestId" />
+  </RouterView>
   <RouterView name="footer" />
 </template>
-
--- vue-router/vue-project/src/components/UserProfile.vue
+++ vue-router/vue-project/src/components/UserProfile.vue
@@ -1,7 +1,13 @@
 <script setup>
+import { defineProps } from "vue";
+
+const { requestId } = defineProps(["requestId"]);
+
+console.log(requestId);
 </script>
 
 <template>
   <h1>User Profile</h1>
   <p>This is the user profile</p>
+  <p>Request ID: {{ requestId }}</p>
 </template>
