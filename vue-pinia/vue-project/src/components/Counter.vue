a5ff3ffab42e8e80c0b8ab125f7665f4fd876509
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-15 14:20:28 +0700

    vue-pinia: Action

--- vue-pinia/vue-project/src/components/Counter.vue
+++ vue-pinia/vue-project/src/components/Counter.vue
@@ -3,23 +3,13 @@ import { useCounter } from "../store";
 
 const counterStore = useCounter();
 
-function handleClick() {
-  counterStore.counter++;
-}
-
 counterStore.$subscribe((mutation, state) => {
   console.log(mutation, state);
 });
-
-function handleReset() {
-  counterStore.$patch({
-    counter: 0,
-  });
-}
 </script>
 
 <template>
   <h1>Counter {{ counterStore.counter }}</h1>
-  <button @click="handleClick">Count</button>
-  <button @click="handleReset">Reset</button>
+  <button @click="counterStore.increment">Count</button>
+  <button @click="counterStore.reset">Reset</button>
 </template>
