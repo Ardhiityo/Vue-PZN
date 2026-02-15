770217496ca791c1855e9b349e60e96f54a8d090
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-15 14:01:06 +0700

    vue-pinia: Mengubah State

--- vue-pinia/vue-project/src/components/Counter.vue
+++ vue-pinia/vue-project/src/components/Counter.vue
@@ -6,9 +6,16 @@ const counterStore = useCounter();
 function handleClick() {
   counterStore.counter++;
 }
+
+function handleReset() {
+  counterStore.$patch({
+    counter: 0,
+  });
+}
 </script>
 
 <template>
   <h1>Counter {{ counterStore.counter }}</h1>
   <button @click="handleClick">Count</button>
+  <button @click="handleReset">Reset</button>
 </template>
